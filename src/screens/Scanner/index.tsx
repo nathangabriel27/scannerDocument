5683b17984b785ac29e3cca0f12c0068fef28620
Nathan Gabriel Almeida Oliveira <nathangabriel27@gmail.com>
2022-09-13 10:48:09 -0300

    Permissao camera android

--- src/screens/Scanner/index.tsx
+++ src/screens/Scanner/index.tsx
@@ -1,22 +1,54 @@
-import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, TouchableOpacity, View } from 'react-native';
+import { request, PERMISSIONS } from 'react-native-permissions';
+
 import { Title } from '../../components/Title';
 import styles from './styles';
 
+type PermissionStatus =
+    | 'unavailable'
+    | 'denied'
+    | 'limited'
+    | 'granted'
+    | 'blocked'
+    | null;
+
 export default function Scanner() {
+    const [permissionResult, setPermissionResult] = useState<PermissionStatus>(null);
+    const [textLoadingProgress, setTextLoadingProgress] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
+    function permissionsValidate() {
+        setTextLoadingProgress('Verificando permissão');
+        setLoading(true);
+        request(
+            Platform.OS === 'ios'
+                ? PERMISSIONS.IOS.CAMERA
+                : PERMISSIONS.ANDROID.CAMERA
+        ).then((result) => {
+            setPermissionResult(result);
+            if (result === 'blocked' || result === 'denied') {
+                console.log(result);
+                setLoading(false);
+            } else {
+                console.log(result);
+                setLoading(false);
+            }
+        });
+    }
     return (
         <View style={styles.container}>
 
-        <TouchableOpacity
-            style={styles.containerButton}
-            activeOpacity={0.8}
-            onPress={() => console.log('onpress')}
-        >
-            <View style={styles.textItem}>
-                <Title text={'item.nome'} />
+            <TouchableOpacity
+                style={styles.containerButton}
+                activeOpacity={0.8}
+                onPress={() => permissionsValidate()}
+            >
+                <View style={styles.textItem}>
+                    <Title text={'item.nome'} />
 
-            </View>
-        </TouchableOpacity>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
