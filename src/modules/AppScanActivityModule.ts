6be5f3a108b5f1ae4b12591ebdebea34b2c3a3f9
dev63.osasbh <pedro.silveira@osas.corp>
2022-09-27 15:16:48 -0300

    working android

--- src/modules/AppScanActivityModule.ts
+++ src/modules/AppScanActivityModule.ts
@@ -1,7 +1,7 @@
 interface IAppScanActivityModule {
     startScan: () => Promise<String>
 }
-
+    
 import { NativeModules } from 'react-native';
 const { AppScanActivityModule } = NativeModules;
 export default AppScanActivityModule as IAppScanActivityModule;
